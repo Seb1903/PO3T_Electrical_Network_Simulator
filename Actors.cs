032252b2136cdd146ae01f58736f29cf3a3932fa
Seb1903 <sebastien.martinez99@gmail.com>
2020-12-07 23:48:59 +0100

    Create C# project and create interface IActor

---
+++ Actors.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+interface IActor
+{
+	public List<Point> placement { get; set; }
+	int area { get; }
+
+	public void setPlacement(List<Point> new_placement)
+	{
+		this.placement = new_placement;
+	}
+
+	public void getPlacement()
+    {
+		return this.placement;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Simulateur_Réseau
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
