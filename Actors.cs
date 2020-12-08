178fb5e4d9cb7cef370418b574caa31c8017e37f
Seb1903 <sebastien.martinez99@gmail.com>
2020-12-08 22:14:20 +0100

    Complete Actors + create graphics class

--- Actors.cs
+++ Actors.cs
@@ -1,17 +1,93 @@
 ﻿using System;
 
-interface IActor
-{
-	public List<Point> placement { get; set; }
-	int area { get; }
 
-	public void setPlacement(List<Point> new_placement)
+namespace Simulateur_Réseau
+{
+	public class Actor
 	{
-		this.placement = new_placement;
+		public List<Point> placement;
+		int area;
+
+		public void setPlacement(List<Point> new_placement)
+		{
+			this.placement = new_placement;
+		}
+
+		public void getPlacement()
+		{
+			return this.placement;
+		}
 	}
 
-	public void getPlacement()
-    {
-		return this.placement;
-    }
+	interface IConsumer
+	{
+		// peut-être créer un int consommation ? 
+		public void Consume(int)
+		{
+			//ajoutera le int à une variable consommation sûrement 
+		}
+
+		public void getPrice()
+		{
+			// retourne le prix de l'électricité utilisée
+		}
+
+		public void getConsumption() {
+			// retourne la consommation, à la seconde près, probablement que Consume sera variable
+		}
+	}
+
+	interface IProducer
+	{
+		// peut-être créer un int production  ? 
+		public void Produce(int)
+		{
+			//ajoutera le int à une variable production sûrement 
+			//surement prendre un deuxième paramètre qui servira de coefficient de variation. 
+		}
+
+		public void getProduce()
+		{
+			// retourne le prix de l'électricité utilisée
+		}
+		public void setcostProduce()  // prix des combustibles à prendre en compte 
+		{
+			// retourne la consommation, à la seconde près, probablement que Consume sera variable
+		}
+		public void getcostProduce()  // prix des combustibles à prendre en compte 
+		{
+			// retourne la consommation, à la seconde près, probablement que Consume sera variable
+		}
+
+		public void getCO2Produced()
+		{
+
+		}
+		public void setCO2Produced() // sûrement essayer de taper ça dans le constructeur (si constructeur possible)
+		{
+
+		}
+	}
+
+	public class City : Actor, IConsumer    //https://stackoverflow.com/questions/56867/interface-vs-base-class
+	{
+
+
+	}
+
+	public class Business : Actor, IConsumer
+	{
+
+	}
+
+	public class Foreign : Actor, IConsumer
+	{
+
+	}
+
+
+	public class Dissipator : Actor, IConsumer
+	{
+
+	}
 }
---
+++ Graphics.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Simulateur_Réseau
+{
+    class Graphics
+    {
+        public string Draw()
+        {
+            string graphics;
+            // parcourir les éléments du grid et par position on écrit dans le string ce que c'est (style ASCII) et on relie par des traits pour faire les lignes électriques 
+            // ex de ce que ça donnerait au final : 
+            /*
+             *   graphics = "    C -------------------V
+             *                   |
+             *                   |
+             *                   P--------V----D-------"
+             *                      
+             *                      on voit que C (une centrale est reliée à une ville (V))
+             *                      on peut utiliser des / et \ pour des diagonales 
+             *                      faut juste paramétrer en fonction de ce qui se trouve dans le grid et sa taille 
+             *                      P = panneaux solaires, D = Dissipateur, etc. 
+             * 
+             * 
+             * 
+             * 
+             * 
+             * */
+            return graphics; 
+        }
+    }
+}
