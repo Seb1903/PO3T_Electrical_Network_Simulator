8fd34b4fb66fd8086561910a7b751c83bc55b77c
Tateuf <logan.noel.h@gmail.com>
2020-12-12 15:57:39 +0100

    Add Node.cs + Line.cs

---
+++ Line.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Simulateur_Réseau
+{
+	public class Line
+	{
+		public Node entrance;
+		public Actor entrance; //peut etre ajouter noeud dans les acteur et rename la classe en lieu de deplacement elec.
+		public Node exit;
+		public Actor exit;
+		public int powerMax;
+		public int powerIn;
+		public int powerNeeded;
+
+		public Line(Actor incoming, )
+		{
+			this.powerMax = powerMax;
+			this.entrance = //
+			this.exit = //
+		}
+
+		public setPowerIn(int powerIn)
+		{
+			this.powerIn = powerIn;
+		}
+
+		public int getPowerIn()
+		{
+			return this.powerIn;
+		}
+
+		public setPowerNeeded(int powerNeeded)
+		{
+			if (powerNeeded < powerMax)
+			{
+				this.powerNeeded = powerNeeded;
+			}
+			else
+			{
+				return; //message d'erreur cette ligne ne peut pas fournir cette énergie.
+			}
+		}
+		public int getPowerNeeded()
+		{
+			return this.powerNeeded;
+		}
+	}
+}
+
---
+++ Node.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Simulateur_Réseau
+{
+	public class Node
+	{
+		public List<Point> placement; //ne reconnait pas encore la classe point !!
+		public List<Line> incomingLine = new List<Line>();
+		public List<Line> outgoingLine = new List<Line>();
+		public int power = 0;
+		public int powerNeeded = 0;
+
+		public delOutgoingLine(Line line)
+		{
+			outgoingLine.remove(line);
+		}
+		public delIncomingLine(Line line)
+		{
+			incomingLine.Remove(line);
+		}
+		public UppdatePower()
+		{
+			foreach (line in incomingLine)
+			{
+				power += line.getPowerIn();
+			}
+			foreach (line in outgoingLine)
+			{
+				powerNeeded += line.getPowerNeeded();
+			}
+			if (powerNeeded <= powerIn)
+			{
+				foreach (line in outgoingLine)
+				{
+					line.setPowerIn(line.getPowerNeeded())
+				}
+			}
+		}
+	}
+	public class distributionNode : Node
+	{
+		public distributionNode(Point placement)
+		{
+			this.placement = placement;
+		}
+		public addIncomingLine(Line line)
+		{
+			if (this.incomingLine.Count == 0)
+			{
+				this.incomingLine.Add(line);
+			}
+			else
+				return; //envoie qu'il y a une erreur
+		}
+		public addOutgoingLine(Line line)
+		{
+			this.outgoingLine.Add(line)
+		}
+	}
+	public class concentrationNode : Node
+	{
+		public concentrationNode(Point placement)
+		{
+			this.placement = placement;
+		}
+		public addIncomingLine(Line line)
+		{
+			this.incomingLine.Add(line);
+		}
+		public addOutgoingLine(Line line)
+		{
+			if (this.outgoingLine.Count == 0)
+			{
+				this.outgoingLine.Add(line);
+			}
+			else
+				return; //envoie qu'il y a une erreur
+		}
+	}
+}
