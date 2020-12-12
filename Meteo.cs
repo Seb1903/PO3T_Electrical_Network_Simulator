95ba7f6dfd44b4dee16e083b5f8bfcfb893d2e6b
Tateuf <logan.noel.h@gmail.com>
2020-12-12 18:10:23 +0100

    Uppdate Meteo.cs +Market.cs

--- Meteo.cs
+++ Meteo.cs
@@ -1,33 +1,38 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
-public class Meteo
+namespace Simulateur_Réseau
 {
-	int temperature;
-	int sunshine;
-	int windForce;
-	
-	
-	public Meteo(int temperature, int sunshine, int windForce, float coeffAléatoire, int variation)
-    {
-		
-		this.temperature = temperature + coeffAléatoire*aleatoire.Next(-variation ; variation) ;
-		this.sunshine = sunshine + coeffAléatoire*aleatoire.Next(-variation ; variation);
-		this.windForce = windForce + coeffAléatoire*aleatoire.Next(-variation ; variation);
-    }
-	public string getMeteo()
-    {
-		return "Température:"+temperature.ToString()+"°C Ensoleillement :"+sunshine.ToString()+"Mj/m^2 Force du vent:"+windForce.ToString()
-    }
-	public int getTemperature()
-    {
-		return temperature;
-    }
-	public int getSunshine()
+	public class Meteo
 	{
-		return sunshine;
+		int temperature;
+		int sunshine;
+		int windForce;
+
+
+		public Meteo(int temperature, int sunshine, int windForce, float coeffAléatoire, int variation)
+		{
+			this.temperature = temperature + coeffAléatoire * aleatoire.Next(-variation ; variation) ;
+			this.sunshine = sunshine + coeffAléatoire * aleatoire.Next(-variation ; variation);
+			this.windForce = windForce + coeffAléatoire * aleatoire.Next(-variation ; variation);
+		}
+		public string getMeteo()
+		{
+			return "Température:" + temperature.ToString() + "°C Ensoleillement :" + sunshine.ToString() + "Mj/m^2 Force du vent:" + windForce.ToString()
+	
 	}
-	public int getWindForce()
-    {
-		return windForce;
-    }
+		public int getTemperature()
+		{
+			return temperature;
+		}
+		public int getSunshine()
+		{
+			return sunshine;
+		}
+		public int getWindForce()
+		{
+			return windForce;
+		}
+	}
 }
